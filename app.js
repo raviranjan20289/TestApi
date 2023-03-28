0e32dc4dcb2d494004340ce3c6252634e5742f1a
diler20289@gmail.com <diler20289@gmail.com>
2023-03-29 00:01:41 +0530

    second changes

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("hello ");
+const express = require("express");
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) =>{
+    res.send("Hello Bhaiya jii");
+});
+ 
+
+const start = async() =>{
+    try{
+        app.listen(PORT, () =>{
+            console.log(`${PORT} yes you are connected now`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+start();
